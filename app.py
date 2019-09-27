e1b977cf49003217fd2c4f581b7e76e965bd8643
Antony Yun <antony.n.yun@gmail.com>
2019-09-27 14:50:42 +0900

    Add sample github api code

---
+++ app.py
@@ -0,0 +1,29 @@
+from secrets import GITHUB_API_KEY
+
+from flask import Flask, render_template, request
+from github import Github
+import json
+
+app = Flask(__name__)
+g = Github(GITHUB_API_KEY)
+
+
+@app.route('/')
+def index():
+    metrics = dict()
+
+    user = request.args.get('user')
+    repos = g.get_user(user).get_repos():
+
+    '''
+    Examples of how to get metrics and return data
+    metrics.update(codequality_metrics(repos))
+    metrics.update(community_metrics(repos))
+    metrics.update(metrics3(repos))
+    '''
+
+    return json.dumps(metrics)
+
+
+if __name__ == '__main__':
+  app.run(port=5000)
