70e36ec9a3fbef54186538eef8068488548148e7
Marcin Matłacz <marcin@matlacz.eu>
2019-09-28 15:38:35 +0900

    Add requirements; Modify endpoint;

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import json
-from functools import lru_cache
-from math import sqrt
 
 from flask import Flask, request
 from github import Github
@@ -17,28 +15,9 @@ g = Github(GITHUB_API_KEY)
 
 @app.route('/')
 def index():
-    user = request.args.get('user')
-    return json.dumps(get(user))
-
-
-@app.route('/compare')
-def compare():
-    userA = request.args.get('userA')
-    userB = request.args.get('userB')
-    resultA = get(userA)
-    resultB = get(userB)
-    similarity = compute_similarity(resultA, resultB)
-    return json.dumps({
-        'userA': resultA,
-        'userB': resultB,
-        'similarity': int(100 * similarity)
-    })
-
-
-@lru_cache(maxsize=10)
-def get(user):
     scores = dict()
 
+    user = request.args.get('user')
     '''
     Examples of how to get metrics and return data
     metrics.update(codequality_metrics(repos))
@@ -53,51 +32,26 @@ def get(user):
     scores['versatility'] = score_versatility(result['top_dependencies'])
     scores['best_practices'] = score_practices(result)
     scores['github_activity'] = score_activity(result)
-    result['overall_score'] = int(100 * compute_overall_score(scores))
-    scores = {k: int(100 * v) for k, v in scores.items()}
 
     comments_score = get_comments_score(g, user)
     pr_score, mean_response_time, all_additions, all_deletions = get_pr_score(g, user)
+
     community_score = (comments_score + pr_score) / 2
 
     scores['community_score'] = community_score
-    result['overall_score'] = int(100 * compute_overall_score(scores))
-    scores = {k: int(100 * v) for k, v in scores.items()}
 
     # Return json
     result['scores'] = scores
 
     result['community_scores'] = {
-        'comments_quality_score': int(comments_score * 100),
-        'pr_quality_score': int(pr_score * 100)
+        'comments_quality_score': comments_score,
+        'pr_quality_score': pr_score
     }
 
-    result['avg_response_time'] = round(mean_response_time, 1)
+    result['avg_response_time'] = mean_response_time
     result['code_additions'] = all_additions
     result['code_deletions'] = all_deletions
-    
-    return result
-
-
-weights = {
-    'versatility': 1,
-    'best_practices': 1,
-    'github_activity': 1,
-}
-
-
-def compute_overall_score(scores):
-    weighted_scores = [weights[key] * scores[key] for key in weights]
-    return sum(weighted_scores) / sum(weights.values())
-
-
-def compute_similarity(resultA, resultB):
-    weightedA = [weights[key] * resultA['scores'][key] for key in weights]
-    weightedB = [weights[key] * resultB['scores'][key] for key in weights]
-    normA = sqrt(sum([s * s for s in weightedA]))
-    normB = sqrt(sum([s * s for s in weightedB]))
-    dot = sum([a * b for a, b in zip(weightedA, weightedB)])
-    return dot / (normA * normB)
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
