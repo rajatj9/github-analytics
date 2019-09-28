992ea8ab8666943e31d10d9277562cbbb7b13580
Rajat Jain <jain.rajat50@gmail.com>
2019-09-28 10:06:52 +0900

    uses_branches

--- results.py
+++ results.py
@@ -13,12 +13,15 @@ def get_results(username: str):
     fork_count = 0
     has_readme = False
     has_maven_gradle = False
+    uses_branches = False
     for repo in repos:
         if "Java" == repo._language.value:
             star_count += repo.stargazers_count
             open_issues += len(list(repo.get_issues(state='open')))
             repo_count += 1
             fork_count += repo._forks_count.value
+            if len(list(repo.get_branches())) > 1:
+                uses_branches = True
             for file in repo.get_dir_contents(""):
                 if file.name == "gradle" or file.name == "pom.xml":
                     has_maven_gradle = True
@@ -29,6 +32,6 @@ def get_results(username: str):
               'location': user._location.value, 'company': user._company.value, 'num_of_java_repos': repo_count,
               'avg_stars_count_per_repo': star_count / repo_count, 'avg_open_issues_per_repo': open_issues / repo_count,
               'avg_fork_count': fork_count / repo_count,
-              'has_maven_gradle': has_maven_gradle, 'has_readme': has_readme}
+              'has_maven_gradle': has_maven_gradle, 'has_readme': has_readme, 'uses_branches': uses_branches}
 
     return result
