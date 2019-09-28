1bd1ebdf945eb9d291a91b8e9a889287116a84c7
Rajat Jain <jain.rajat50@gmail.com>
2019-09-28 22:41:11 +0900

    cqi calc fix

--- results.py
+++ results.py
@@ -34,7 +34,8 @@ def get_results(username: str):
                     has_readme = True
             if analyzed_repos < 2:
                 repo_stats = get_repo_stats(repo.git_url)
-                code_quality_index += round(repo_stats['insertions']+repo_stats['deletions'] / repo_stats['total_lines'], 2)
+                # repo_stats = {'files_changes': 4, 'insertions': 4, 'deletions': 21, 'total_files': 72, 'total_lines': 14828}
+                code_quality_index += round((repo_stats['insertions']+repo_stats['deletions']) / repo_stats['total_lines'], 2)
             analyzed_repos += 1
 
 
