1bda182794ba7a351b927dad6ebb60c32366555f
Marcin Matłacz <marcin@matlacz.eu>
2019-09-28 15:38:35 +0900

    Add requirements; Modify endpoint;

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from dependencies.dependencies import get_top_dependencies, score_versatility
-from secrets import GITHUB_API_KEY
-
-from flask import Flask, render_template, request
-from github import Github
-from results import get_results, score_practices, score_activity
 import json
+
+from flask import Flask, request
+from github import Github
+
+from dependencies.dependencies import get_top_dependencies, score_versatility
+from results import get_results, score_practices, score_activity
+from scorers.CommentsCommunityEngagemnt import get_comments_score
+from scorers.PullRequestScore import get_pr_score
+from secrets import GITHUB_API_KEY
 
 app = Flask(__name__)
 g = Github(GITHUB_API_KEY)
@@ -30,8 +33,24 @@ def index():
     scores['best_practices'] = score_practices(result)
     scores['github_activity'] = score_activity(result)
 
+    comments_score = get_comments_score(g, user)
+    pr_score, mean_response_time, all_additions, all_deletions = get_pr_score(g, user)
+
+    community_score = (comments_score + pr_score) / 2
+
+    scores['community_score'] = community_score
+
     # Return json
     result['scores'] = scores
+
+    result['community_scores'] = {
+        'comments_quality_score': comments_score,
+        'pr_quality_score': pr_score
+    }
+
+    result['avg_response_time'] = mean_response_time
+    result['code_additions'] = all_additions
+    result['code_deletions'] = all_deletions
     return json.dumps(result)
 
 
